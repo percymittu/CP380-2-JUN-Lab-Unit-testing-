93876f38e0a2c05aa00c4c1eaddde678b639d78c
Percy Mittagadapala <willi.percy3@gmail.com>
2021-08-01 00:05:20 -0400

    IsItALeapYear

---
+++ IsItALeapYear/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsItALeapYear
+{
+    public class Program
+    {
+        public bool IsItALeapYear(int yearPassed)
+        {
+            if ((yearPassed % 4 == 0 && yearPassed % 100 != 0) || (yearPassed % 400 == 0))
+                return true;
+            return false;
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ IsItALeapYearUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IsItALeapYearUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IsItALeapYearUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b393abdd-03f7-42d7-a54f-09548443bc9e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
